b5be64b21451b95c3261e4e221a91247990065c7
Michael Sumner <mvsumner@mac.com>
2017-09-27 02:35:21 +0000

    created chess board

--- src/js/initBoard.js
+++ src/js/initBoard.js
@@ -1,10 +1,28 @@
 function initBoard () {
     console.log('initBoard');
-    
-    for(let i = 0;i < 8;i++) {
-        const row = document.createElement('div');
-    }
-    
-}
 
+    const board = document.getElementById('chess-board');
+    
+    for(let x = 0;x < 8;x++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let y =0; y <8; y++) {
+            const space = document.createElement('div');
+                if(x % 2 ===0) {
+                y % 2 === 0 ? 
+                space.classList.add('white-space') :
+                space.classList.add('black-space');
+                
+            } else {
+                y % 2 === 0 ?
+                space.classList.add('black-space') :
+                space.classList.add('white-space');
+                
+            }
+            row.appendChild(space);
+        }
+        board.appendChild(row);
+    }
+
+}
 export default initBoard;
