9407420d6fb25be4fd1a21f84b78947dcc1be0b0
Michael Sumner <mvsumner@mac.com>
2017-10-25 03:33:09 +0000

    working game

---
+++ src/js/state.js
@@ -0,0 +1,13 @@
+const state = {
+    currTurn: 'wht',
+    moved: false,
+    southPlayer: 'blk',
+    currSelect: '',
+    currPiece: '',
+    previousPos: '',
+    capture: '',
+    time: '00:00:00',
+    // availableMoves: [],
+}
+
+export default state;
